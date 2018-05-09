deb867ee8bf69d0fe189f0fba52202c9c3b6c2f9
Eesh Kant Joshi <eesh.joshi@gmail.com>
2018-05-09 16:05:40 +0530

    Initial commit

---
+++ Linked Lists/Linked List Deletion/LinkedList.java
@@ -0,0 +1,97 @@
+public class LinkedList
+{
+    Node head;
+
+    /* Inner class is made static so that main() can access it */
+    static class Node
+    {
+        int data;
+        Node next;
+
+        /* Contructor */
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Function to print all the node values of the Linked List */
+    public void print_nodes()
+    {
+        /* Linked List traversal */
+        Node n = head;
+
+        int i=1;
+        while(n != null)
+        {
+            System.out.println("Node " + i++ + " value => " + n.data);
+            n = n.next;
+        }
+
+        return;
+    }
+
+    public void delete_node(int key)
+    {
+        Node temp = head;
+        Node prev = null;
+        
+        /* If the key == value of first node */
+        if(temp != null && temp.data == key)
+        {
+            head = head.next;
+            return;
+        }
+
+        /* Searching the Linked List for the key  */
+        while(temp != null)
+        {
+            if(temp.data == key)
+            {
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+
+        /* Key not found in the Linked List */
+        if(temp == null)
+        {
+            return;
+        }
+
+        /* Deleting the temp node */
+        prev.next = temp.next;
+        
+        return;
+    }
+
+    
+    public static void main(String[] args)
+    {
+
+        /* Create an empty list */
+        LinkedList llist = new LinkedList();
+
+        /* Adding 3 nodes with values */
+        llist.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        /* Connecting the nodes */
+        llist.head.next = second;
+        second.next = third;
+
+        /* Before Deleting */
+        System.out.println("Before Deleting");
+        llist.print_nodes(); 
+        
+        /* Deleting node with value 2 */
+        System.out.println("\nDeleting node with value 2");
+        llist.delete_node(2);
+
+    }
+}
+
+
+
