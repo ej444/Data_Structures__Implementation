32215f2ea190bc570cf78e3d907d71e5b2462e47
Eesh Kant Joshi <eesh.joshi@gmail.com>
2018-05-14 14:27:48 +0530

    Add files via upload

---
+++ Linked Lists/7. Merge two sorted LL/Merge.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Merge extends LinkedList
+{
+    public static void main(String[] args)
+    {
+        /* Making two linked lists llist1 and llist2 */   
+        LinkedList llist1 = new LinkedList();
+        LinkedList llist2 = new LinkedList();
+        
+        llist1.head = new Node(1);
+        Node n12 = new Node(3);
+        Node n13 = new Node(5);
+
+        llist1.head = new Node(2);
+        Node n22 = new Node(4);
+        Node n23 = new Node(6);
+        Node n24 = new Node(8);
+        
+
+    }
+}
