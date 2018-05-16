4bce114cd041d95202138ed859cd9dad57cf87cc
Eesh Kant Joshi <eesh.joshi@gmail.com>
2018-05-16 09:39:22 +0530

    New question

---
+++ Linked Lists/8. Reverse a Linked List in groups of given size/Merge.java
@@ -0,0 +1,62 @@
+/* Linked List collection in Java
+
+// Importing libraries
+import java.util.LinkedList;
+
+// Initializing a LL
+List<Integer> llist = new LinkedList<Integer>();
+
+// LL methods
+llist.add("item") >> To add a new item
+llist.get(n) >> To get the nth(starting from 0) element of the LL
+
+llist.addFirst("first_item")/llist.addLast("last_item") >> Adds the node at the starting/ending of LL
+llist.deleteFirst()/llist.deleteLast() >> Deletes the node at the starting/ending of LL
+ * add() and addLast() do the same thing *
+
+llist.add(position, "item") >> Adds the items at a  given position
+llist.delete(position) >> Deletes the node at the given position
+
+// Display the whole LL
+System.out.println(llist)
+
+ */
+
+import java.io.*;
+import java.util.List;
+import java.util.LinkedList;
+
+public class Merge
+{
+    public static void main(String[] args)
+    {
+        LinkedList<Integer> llist = new LinkedList<Integer>();
+        llist.add(3);
+        llist.add(2);
+        llist.add(1);
+        llist.add(6);
+        llist.add(5);
+        llist.add(4);
+        
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
