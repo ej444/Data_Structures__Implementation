ba73ccd8fba5ad8feae4480e27aafabec141ff43
ej444 <eesh.joshi@gmail.com>
2018-06-13 19:55:11 +0530

    Debugging

--- 3. Stack/4. Next Greater Element/Main.java
+++ 3. Stack/4. Next Greater Element/Main.java
@@ -9,7 +9,27 @@ public class Main
 
 	void printNGE(int[] arr, int n)
 	{
+		int max = arr[n-1]; 
+		arr[n-1] = -1;
 
+		for(int i = n-2; i>=0; i++)
+		{
+			if(arr[i] > max)
+			{
+				max = arr[i];
+				arr[i] = -1;
+			}
+			else
+			{
+				arr[i] = max;
+			}
+		}
+
+		System.out.printf("The Next Greater Element array is: ")
+		for(int j : arr)
+		{
+			System.out.printf(j + " ");
+		}
 	}
 	
 }
