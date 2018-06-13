34f44f5aa9d168ede74c19536497386551f48512
ej444 <eesh.joshi@gmail.com>
2018-06-13 19:41:54 +0530

    Adding Basic structure

--- 3. Stack/4. Next Greater Element/Main.java
+++ 3. Stack/4. Next Greater Element/Main.java
@@ -1,4 +1,17 @@
-public class Main
+import java.io.*;
+
+public class Main extends Stack
 {
+	public static void main(String[] args)
+	{
+		int[] arr = {12, 15, 10, 11, 2}
+
+		printNGE(arr, arr.length);
+	}
+
+	void printNGE(int[] arr, int n)
+	{
+		
+	}
 	
 }
