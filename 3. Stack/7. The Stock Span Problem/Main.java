f03a3656ccdfe0d563e96978614c8a5b44f4bb4b
ej444 <eesh.joshi@gmail.com>
2018-06-15 19:53:02 +0530

    modifying

--- 3. Stack/7. The Stock Span Problem/Main.java
+++ 3. Stack/7. The Stock Span Problem/Main.java
@@ -6,15 +6,27 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		int[] arr = {10, 4, 5, 90, 120, 80};
-		int[] span_values = new int[arr.length];
-		s.push(arr[0]);
-		SSP(arr, span_values);
+		int[] price = {10, 4, 5, 90, 120, 80};
+		int[] span_values = new int[price.length];
+
+		// Note: Pushing index rather than the value
+		s.push(0);
+		SSP(price, span_values);
 		System.out.printf("The span values are: ");
 		for(int i : span_values)
 		{
 			System.out.printf(i + " ");
 		}	
+	}
+
+	public static void SSP(int[] price, int[] span_values)
+	{
+		price[0] = 1;
+
+		for(int i = 1; i < price.length; i++)
+		{
+
+		}
 	}
 }
 
