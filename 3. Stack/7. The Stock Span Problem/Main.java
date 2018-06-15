8c32b59f9a5b073b7d8efb6a5a1379e3f4bbaff8
ej444 <eesh.joshi@gmail.com>
2018-06-15 18:59:48 +0530

    New implementation

---
+++ 3. Stack/7. The Stock Span Problem/Main.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class Main
+{
+	public static Stack<Integer> s = new Stack<>();
+
+	public static void main(String[] args)
+	{
+		int[] arr = {10, 4, 5, 90, 120, 80};
+
+		
+
+		
+	}
+}
+
+
+/* OUTPUT
+
+
+*/
