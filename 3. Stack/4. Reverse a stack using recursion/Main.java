25e9dc4db441cc8ab7896f44595bb97448f07d09
ej444 <eesh.joshi@gmail.com>
2018-06-15 15:26:35 +0530

    debugging

--- 3. Stack/4. Reverse a stack using recursion/Main.java
+++ 3. Stack/4. Reverse a stack using recursion/Main.java
@@ -5,7 +5,6 @@ public class Main
 	public static void main(String[] args)
 	{
 		Stack<String> s = new Stack<>();
-		Stack<String> temp = new Stack<>();
 		s.push("1");
 		s.push("2");
 		s.push("3");
@@ -25,11 +24,11 @@ public class Main
 
 	public static void reverse(Stack s)
 	{
-		if(!s.isEmpty())
-		{
-			String str = (String) s.pop();
-			reverse(s);
-			s.push(str);
-		}
+
+	}
+
+	public static insert_at_bottom(String str)
+	{
+
 	}
 }
