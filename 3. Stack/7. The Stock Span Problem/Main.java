606b6fae85e7f9957b013ef53a9bcabb7a7a5e10
ej444 <eesh.joshi@gmail.com>
2018-06-15 19:04:34 +0530

    Main fn completed

--- 3. Stack/7. The Stock Span Problem/Main.java
+++ 3. Stack/7. The Stock Span Problem/Main.java
@@ -7,10 +7,14 @@ public class Main
 	public static void main(String[] args)
 	{
 		int[] arr = {10, 4, 5, 90, 120, 80};
-
-		
-
-		
+		int[] span_values = new int[arr.length];
+		s.push(arr[0]);
+		SSP(arr, span_values);
+		System.out.printf("The span values are: ");
+		for(int i : span_values)
+		{
+			System.out.printf(i + " ");
+		}	
 	}
 }
 
