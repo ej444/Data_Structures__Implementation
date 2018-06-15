3e325adad0cdd1f8813c53eb9732b95341220a1d
ej444 <eesh.joshi@gmail.com>
2018-06-15 15:53:10 +0530

    Final solution

--- 3. Stack/4. Reverse a stack using recursion/Main.java
+++ 3. Stack/4. Reverse a stack using recursion/Main.java
@@ -1,10 +1,25 @@
-import java.util.*;
+/* 
+import java.util.*; ...... gives error
+Main.java:5: error: type Stack does not take parameters
+        public static Stack<String> s = new Stack<>();
+                           ^
+Main.java:5: error: cannot infer type arguments for Stack
+        public static Stack<String> s = new Stack<>();
+                                                 ^
+  reason: cannot use '<>' with non-generic class Stack
+2 errors
+
+But import.java.util.Stack; does not.
+*/
+
+import java.util.Stack;
 
 public class Main
 {
+	public static Stack<String> s = new Stack<>();
+
 	public static void main(String[] args)
 	{
-		Stack<String> s = new Stack<>();
 		s.push("1");
 		s.push("2");
 		s.push("3");
@@ -17,18 +32,39 @@ public class Main
 		}
 
 		System.out.println("\nInitial stack with top = "+ s.peek() + ">> " + s);
-		reverse(s);
+		reverse();
 		System.out.println("\nFinal stack with top = "+ s.peek() + ">> " + s);
 
 	}
 
-	public static void reverse(Stack s)
+	// Function to go at the bottom of the stack
+	public static void reverse()
 	{
+		if(!s.isEmpty())
+		{
+			String str = s.pop();
+			reverse();
+			insert_at_bottom(str);
+		}
 
 	}
 
-	public static insert_at_bottom(String str)
+
+	//This function places the incoming item at the bottom of the existing stack
+	public static void insert_at_bottom(String str)
 	{
+		// Empties the whole stack and at last push the incoming item.
+		if(s.isEmpty())
+		{
+			s.push(str);
+		}
+		else
+		{
+			// Keep pushing other elements of the stack after the incoming item has been push to the last. 
+			String temp = s.pop();
+			insert_at_bottom(str);
+			s.push(temp);
+		}
 
 	}
 }
