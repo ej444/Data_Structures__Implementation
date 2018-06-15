a5c0ea7e8a45f9ae4963aee8c8b1a3d51730ce8c
ej444 <eesh.joshi@gmail.com>
2018-06-15 20:11:04 +0530

    final touch

--- 3. Stack/7. The Stock Span Problem/Main.java
+++ 3. Stack/7. The Stock Span Problem/Main.java
@@ -12,6 +12,12 @@ public class Main
 		// Note: Pushing index rather than the value
 		s.push(0);
 		SSP(price, span_values);
+		System.out.printf("The price values are: ");
+		for(int i : price)
+		{
+			System.out.printf(i + " ");
+		}
+		System.out.println("\n");
 		System.out.printf("The span values are: ");
 		for(int i : span_values)
 		{
@@ -21,15 +27,18 @@ public class Main
 
 	public static void SSP(int[] price, int[] span_values)
 	{
+		// The first value will have span = 1
 		span_values[0] = 1;
 
 		for(int i = 1; i < price.length; i++)
 		{
+			// If smaller previous elements found, it is popping the stack
 			while(!s.isEmpty() && price[s.peek()] <= price[i])
 			{
 				s.pop();	
 			}
 
+			// Distances b/w the indexes is subtracted
 			span_values[i] = s.isEmpty() ? i+1 : i - s.peek();
 			s.push(i);
 		}
@@ -39,5 +48,8 @@ public class Main
 
 /* OUTPUT
 
+The price values are: 10 4 5 90 1
+
+The span values are: 1 1 2 4 5 1
 
 */
