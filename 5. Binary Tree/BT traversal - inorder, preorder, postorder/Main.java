accc77248b2a7ffd12cad3babbf23a22798b13e5
Eesh Kant Joshi <eesh.joshi@gmail.com>
2018-06-29 14:40:20 +0530

    Update Main.java

--- 5. Binary Tree/BT traversal - inorder, preorder, postorder/Main.java
+++ 5. Binary Tree/BT traversal - inorder, preorder, postorder/Main.java
@@ -11,8 +11,17 @@ public class Main extends BT
 		b.root.left.left = new Node(4);
 		b.root.left.right = new Node(5);
 
+		System.out.print("\nInorder traversal: ");
 		printInorder(b.root);
+		// Backspace does not delete the content, it just shifts the cursor to the left and then the content can be cleared by adding spaces
+		System.out.print("\b\b" + "  ");
+
+		System.out.print("\nPreorder traversal: ");
 		printPreorder(b.root);
+  		System.out.print("\b\b" + "  ");
+
+		System.out.print("\nPostorder traversal: ");
 		printPostorder(b.root);
+		System.out.print("\b\b" + "  ");
 	}
 }
