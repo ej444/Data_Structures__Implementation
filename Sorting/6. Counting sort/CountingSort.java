4610b04b8287bd72c13d07d190a85534632aba80
ej444 <eesh.joshi@gmail.com>
2018-09-12 10:06:04 -0400

    counting sort

---
+++ Sorting/6. Counting sort/CountingSort.java
@@ -0,0 +1,22 @@
+public class CountingSort
+{
+	public static void main(String[] args)
+	{
+		int[] arr = [5,4,1,4,3,2,1];
+		sort(arr);
+	}
+
+	public static void sort(int[] a)
+	{
+		// c array indices are the range of numbers in which our i/p array lies. We initialize the elements to 0.
+		int[] c = new int[10];
+		for(int i=0; i<10; i++)
+		{
+			c[i] = 0;
+		}
+
+
+
+
+	}
+}
